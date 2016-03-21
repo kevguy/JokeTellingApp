d64b9c19861e6d13ba78985f20b37263342396c4
kevguy <kevlai22@uw.edu>
2016-03-21 14:59:25 +0800

    feat: Added Interstitial Ad

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,12 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.example.android.jokefactory.DisplayJokeActivity;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
+import com.udacity.gradle.builditbigger.R;
 
 
 /**
@@ -26,10 +31,51 @@ public class MainActivityFragment extends Fragment {
     ProgressBar progressBar = null;
     public String loadedJoke = null;
     public boolean testFlag = false;
+    PublisherInterstitialAd mPublisherInterstitialAd = null;
+    String LOG_TAG = "FREEDUBUG";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Set up for pre-fetching interstitial ad request
+        mPublisherInterstitialAd = new PublisherInterstitialAd(getContext());
+        mPublisherInterstitialAd.setAdUnitId("ca-app-pub-7867604826748291/8122977163");
+
+        mPublisherInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                //process the joke Request
+                progressBar.setVisibility(View.VISIBLE);
+                getJoke();
+
+                //pre-fetch the next ad
+                requestNewInterstitial();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+
+                Log.i(LOG_TAG, "onAdFailedToLoad: ad Failed to load. Reloading...");
+
+                //prefetch the next ad
+                requestNewInterstitial();
+
+            }
+
+            @Override
+            public void onAdLoaded() {
+                Log.i(LOG_TAG, "onAdLoaded: interstitial is ready!");
+                super.onAdLoaded();
+            }
+        });
+
+        //Kick off the fetch
+        requestNewInterstitial();
+
+
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
@@ -39,8 +85,14 @@ public class MainActivityFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
-                getJoke();
+                if (mPublisherInterstitialAd.isLoaded()) {
+                    Log.i(LOG_TAG, "onClick: interstitial was ready");
+                    mPublisherInterstitialAd.show();
+                } else {
+                    Log.i(LOG_TAG, "onClick: interstitial was not ready.");
+                    progressBar.setVisibility(View.VISIBLE);
+                    getJoke();
+                }
             }
         });
 
@@ -72,6 +124,16 @@ public class MainActivityFragment extends Fragment {
             context.startActivity(intent);
             progressBar.setVisibility(View.GONE);
         }
+    }
+
+
+    private void requestNewInterstitial() {
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder()
+                //.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("EA27D37DF5448BF42AA5F7A6D4F11A9B")
+                .build();
+
+        mPublisherInterstitialAd.loadAd(adRequest);
     }
 
 }
